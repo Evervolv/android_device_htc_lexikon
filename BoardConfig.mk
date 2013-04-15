2a4e22c77d8fef38bfae6e7b08fa5c72ea22d546
Nick Reuter <nreuter85@gmail.com>
2013-04-15 05:12:47 +0000

    Revert "Lexikon: move specific kernel defines to common"
    This reverts commit bf225338bc4c5efd0f5b61c4e153375bbb58d1b6
    
    Change-Id: Ic4ac35012e72d3c128f725ddd3efca18e2751d48

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,6 +51,7 @@ BOARD_KERNEL_PAGE_SIZE := 4096
 
 TARGET_KERNEL_SOURCE := kernel/htc/msm7x30
 TARGET_KERNEL_CONFIG := evervolv_lexikon_defconfig
+KERNEL_TOOLCHAIN_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-linux-linaro-4.7/bin/arm-eabi-
 
 BOARD_USES_GENLOCK := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := lexikon
